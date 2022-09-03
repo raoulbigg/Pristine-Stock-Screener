a7172333c0194381a4dc989bd7957828056302ae
raoulbigg <raoulbiggelaar@gmail.com>
2022-09-03 13:12:50 +0200

    Update marketoverview.py

--- marketoverview.py
+++ marketoverview.py
@@ -3,10 +3,6 @@ from EdgeWatch.edgewatch import *
 #Stock market overview
 Marketoverview = MarketOverview().todays_markets_overview()
 
-#Screening for edge
-#pairs = open('15m_pairs.txt','r').readlines()
-hourly_pairs = open('hourly_pairs.txt','r').readlines()
-daily_pairs = open('pairs.txt','r').readlines()
 
-#screener = Screener(tickers=pairs, timeframe="15m").start_stock_screener()
-screener = Screener(tickers=hourly_pairs, daily_tickers=daily_pairs, timeframe="1h").start_stock_screener()
+daily_pairs = open('pairs.txt','r').readlines()
+screener = Screener(daily_tickers=daily_pairs, timeframe="1d").start_stock_screener()
