7066dafe1833723bcf6bb2bc5a182df27a6c93a6
raoulbigg <raoulbiggelaar@gmail.com>
2022-09-19 10:40:09 +0200

    Get pairs from Etoro

--- screener.py
+++ screener.py
@@ -1,8 +1,15 @@
 from EdgeWatch.edgewatch import *
+from EdgeWatch.marketoverview.symbols import *
 
 #Stock market overview
-Marketoverview = MarketOverview().todays_markets_overview()
+#Marketoverview = MarketOverview().todays_markets_overview()
+
+#Get all US markets on Etoro.com
+ETORO = Symbols().etoroSymbols()
+pairs = Symbols().etoroUS(ETORO)
 
 
-pairs = open('pairs.txt','r').readlines()
-screener = Screener(tickers=pairs, timeframe="1d").start_stock_screener()
+#pairs = open('pairs.txt','r').readlines()
+
+#Start screener on All mode (calculates POP and priceaction)
+screener = Screener(tickers=pairs, timeframe="1d", mode="All").start_stock_screener()
