0dace4d8272893e9a186ecfca0b415fb3dc9a7e2
raoulbigg <raoulbiggelaar@gmail.com>
2022-09-19 10:59:07 +0200

    Add 8ma to pop

--- EdgeWatch/pattern_elements/calc_pop.py
+++ EdgeWatch/pattern_elements/calc_pop.py
@@ -1,19 +1,21 @@
 def calc_picture_of_power(data):
 	close = data.iloc[0]["Close"]
+	sma = data.iloc[0]["SMA_8"]
 	short_sma = data.iloc[0]["SMA_20"]
 	medium_sma = data.iloc[0]["SMA_40"]
 
 	close1 = data.iloc[1]["Close"]
+	sma1 = data.iloc[1]["SMA_8"]
 	short_sma1 = data.iloc[1]["SMA_20"]
 	medium_sma1 = data.iloc[1]["SMA_40"]
 	
 
-	#calc +POP
-	if short_sma > medium_sma and close > short_sma:
-		if medium_sma > medium_sma1 and close / medium_sma <= 1.2:
+	#calc +POP (if 8ma > 20ma and 20ma > 40ma)
+	if close > short_sma:
+		if sma > short_sma and short_sma > medium_sma and short_sma > short_sma1 and close / short_sma <= 1.1:
 			return "up"
-	#calc -POP
-	elif short_sma < medium_sma and close < short_sma:
-		if medium_sma < medium_sma1 and medium_sma / close <= 1.2:
+	#calc -POP (if 8ma < 20ma and 20ma is declining)
+	elif close < short_sma:
+		if sma < short_sma and short_sma < medium_sma and short_sma < short_sma1 and close / short_sma <= 1.1:
 			return "down"
 	return
