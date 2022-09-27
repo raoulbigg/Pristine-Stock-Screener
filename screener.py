c78afe267dd9e601061ef74fb62f3a541d38cc81
raoulbigg <raoulbiggelaar@gmail.com>
2022-09-27 08:48:33 +0200

    Update screener.py

--- screener.py
+++ screener.py
@@ -2,14 +2,11 @@ from EdgeWatch.edgewatch import *
 from EdgeWatch.marketoverview.symbols import *
 
 #Stock market overview
-#Marketoverview = MarketOverview().todays_markets_overview()
+Marketoverview = MarketOverview().todays_markets_overview()
 
 #Get all US markets on Etoro.com
 ETORO = Symbols().etoroSymbols()
 pairs = Symbols().etoroUS(ETORO)
-
-
-#pairs = open('pairs.txt','r').readlines()
 
 #Start screener on All mode (calculates POP and priceaction)
 screener = Screener(tickers=pairs, timeframe="1d", mode="All").start_stock_screener()
