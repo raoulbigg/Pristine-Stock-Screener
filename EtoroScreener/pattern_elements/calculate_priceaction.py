4f39fb241d0fe2b9dbc62efa2ee09af6b54ac3db
raoulbigg <raoulbiggelaar@gmail.com>
2022-10-24 10:16:06 +0200

    Update calculate_priceaction.py

--- EtoroScreener/pattern_elements/calculate_priceaction.py
+++ EtoroScreener/pattern_elements/calculate_priceaction.py
@@ -8,60 +8,54 @@ def calc_bars_positions(data, pop, timeframe):
 
 	if pop == "up":
 		#Check if bullish engulfing
-		if current["Close"] > current["Open"] and current["Close"] > barTwo["Close"]:
+		if current["Close"] > current["Open"] and current["Low"] < barTwo["Low"]:
 			#Looks at first open above engulfing bar. And look if a void is present.
 			for i in range(30):
 				if i != 0:
 					if data.iloc[i]["Open"] > current["Close"]:
 						if data.iloc[i+1]["Close"] < data.iloc[i+1]["Open"]:
-							if data.iloc[i+2]["Close"] < data.iloc[i+2]["Open"]:
-								return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
+							return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
 						else:
 							break
 			for i in range(30):
 				if i != 0:
 					if data.iloc[i]["Open"] > current["Close"]:
 						if data.iloc[i-1]["Close"] < data.iloc[i-1]["Open"]:
-							if data.iloc[i-2]["Close"] < data.iloc[i-2]["Open"]:
-								return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
+							return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
 						else:
 							break
 			for i in range(30):
 				if i != 0:
 					if data.iloc[i]["Open"] < current["Close"]:
 						if data.iloc[i-1]["Close"] < data.iloc[i-1]["Open"]:
-							if data.iloc[i+1]["Close"] < data.iloc[i+1]["Open"]:
-								return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
+							return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
 						else:
 							break
 
 
 	if pop == "down":
 		#Check if bearish engulfing
-		if current["Close"] < current["Open"] and current["Close"] < barTwo["Close"]:
+		if current["Close"] < current["Open"] and current["High"] > barTwo["High"]:
 			#Looks at first open below engulfing bar. And look if a void is present.
 			for i in range(30):
 				if i != 0:
 					if data.iloc[i]["Open"] < current["Close"]:
 						if data.iloc[i+1]["Close"] > data.iloc[i+1]["Open"]:
-							if data.iloc[i+2]["Close"] > data.iloc[i+2]["Open"]:
-								return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
+							return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
 						else:
 							break
 			for i in range(30):
 				if i != 0:
 					if data.iloc[i]["Open"] < current["Close"]:
 						if data.iloc[i-1]["Close"] > data.iloc[i-1]["Open"]:
-							if data.iloc[i-2]["Close"] > data.iloc[i-2]["Open"]:
-								return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
+							return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
 						else:
 							break
 			for i in range(30):
 				if i != 0:
 					if data.iloc[i]["Open"] < current["Close"]:
 						if data.iloc[i-1]["Close"] > data.iloc[i-1]["Open"]:
-							if data.iloc[i+1]["Close"] > data.iloc[i+1]["Open"]:
-								return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
+							return "180bar:",current["Close"], "barTwo:", barTwo["Close"]
 						else:
 							break
 
