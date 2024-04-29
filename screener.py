e43177fb289167a9cf2d05550bf7880ca9dc65d9
raoulbigg <raoulbiggelaar@gmail.com>
2024-04-29 09:42:37 +0200

    Update screener.py

--- screener.py
+++ screener.py
@@ -16,19 +16,11 @@ def get_hourly_stocks():
 while 1 > 0:
 	now = datetime.datetime.now()
 
-	#Start 1d + 1h screener
+	#Start 1d screener
 	if (now.now().hour == 21 and now.now().minute == 59):
 		screener = Screener(tickers=USsymbols, timeframe="1d").start_stock_screener()
-		#get hourly stocks for 1h screen
-		USstocks500k = get_hourly_stocks()
-		screener = Screener(tickers=USstocks500k, timeframe="1h").start_stock_screener()
-
-	#Start 1h screener
-	if (now.now().hour == 16 and now.now().minute == 10 or
-		now.now().hour == 17 and now.now().minute == 10 or
-		now.now().hour == 18 and now.now().minute == 10):
-		#get hourly stocks for 1h screen
-		USstocks500k = get_hourly_stocks()
-		screener = Screener(tickers=USstocks500k, timeframe="1h").start_stock_screener()	
-	time.sleep(10)
+			#get hourly stocks for 1h screen
+			#USstocks500k = get_hourly_stocks()
+			#screener = Screener(tickers=USstocks500k, timeframe="1h").start_stock_screener()
+		time.sleep(10)
 #screener takes ~30min to finish
