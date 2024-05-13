01b06448aca5463b898b85ff998c76b773713ab4
raoulbigg <raoulbiggelaar@gmail.com>
2024-05-13 20:12:20 +0200

    Update calc_pop.py

--- PristineScreener/pattern_elements/calc_pop.py
+++ PristineScreener/pattern_elements/calc_pop.py
@@ -3,26 +3,25 @@ def calc_picture_of_power(data):
 	close = data.iloc[0]["Close"]
 	high = data.iloc[0]["High"]
 	low = data.iloc[0]["Low"]
-	very_short_sma = data.iloc[0]["SMA_13"]
+	very_short_sma = data.iloc[0]["SMA_10"]
 	short_sma = data.iloc[0]["SMA_20"]
-	medium_sma = data.iloc[0]["SMA_50"]
 
 	#Bar 2
 	close2 = data.iloc[2]["Close"]
 	high2 = data.iloc[2]["High"]
 	low2 = data.iloc[2]["Low"]
+	very_short_sma2 = data.iloc[2]["SMA_10"]
 	short_sma2 = data.iloc[2]["SMA_20"]
-	medium_sma2 = data.iloc[2]["SMA_50"]
 	
 
 	#calc +POP
-	if close > short_sma and close > medium_sma:
-		if short_sma > short_sma2 and medium_sma > medium_sma2:
+	if close > short_sma and very_short_sma > short_sma:
+		if short_sma > short_sma2 and very_short_sma > very_short_sma2:
 			return "up"
 
 	#calc -POP
-	if close < short_sma and close < medium_sma:
-		if short_sma < short_sma2 and medium_sma < medium_sma2:
+	if close < short_sma and very_short_sma < short_sma :
+		if short_sma < short_sma2 and very_short_sma < very_short_sma2:
 			return "down"
 
 	return
