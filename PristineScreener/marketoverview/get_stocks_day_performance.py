d63bc4a5b9553f53c9af2a065656d38badaf8e5b
raoulbigg <raoulbiggelaar@gmail.com>
2024-05-18 12:46:08 +0200

    Update get_stocks_day_performance.py

--- PristineScreener/marketoverview/get_stocks_day_performance.py
+++ PristineScreener/marketoverview/get_stocks_day_performance.py
@@ -23,8 +23,8 @@ def get_stock_losers():
             top_losers.append(percent["data-symbol"] +' \033[91m'+ percent.get_text()+'\033[0m')
     return top_losers
 
-def getMarketInfo():
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    r = requests.get("https://www.tradinghours.com/open?", headers=headers)
-    soup = BeautifulSoup(r.text)
-    return soup.findAll('p', {'class': 'lead text-center mb-5 font-weight-bold'})[0].text
+#def getMarketInfo():
+#    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+#    r = requests.get("https://www.tradinghours.com/open?", headers=headers)
+#    soup = BeautifulSoup(r.text)
+#    return soup.findAll('p', {'class': 'lead text-center mb-5 font-weight-bold'})[0].text
