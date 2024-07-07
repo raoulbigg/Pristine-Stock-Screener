2b81dfda5d391f993e3741223e0331d0ada6bee3
raoulbigg <raoulbiggelaar@gmail.com>
2024-07-07 07:42:07 +0200

    Update app.py

--- flaskSite/app.py
+++ flaskSite/app.py
@@ -16,29 +16,6 @@ def home():
     print(meta_json)
     return render_template('index.html', images=images, meta=meta_json, mode="screen", img_src="static")
 
-@app.route('/hourly')
-def hourly():
-    images = []
-    for file in os.listdir('static/hourly'):
-        if file.endswith('.png'):
-            images.append(file)
-    meta = open("static/meta1h.json")
-    meta_json = json.load(meta)
-    print(meta_json)
-    return render_template('index.html', images=images, meta=meta_json, mode="screen", img_src="static/hourly")
-
-@app.route('/watchlist', methods=['POST'])
-def watchlist():
-    images = []
-
-
-    x = request.form['tickers'].replace(' ', '')
-    tickers_list = list(x.split(","))
-    for ticker in tickers_list:
-        images.append(ticker+".png")
-
-    return render_template('index.html', images=images, mode="watchlist")
-
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="192.168.1.98")
